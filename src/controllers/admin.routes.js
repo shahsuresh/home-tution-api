f9f7d3ced5fa02f110e5d2659130617f64d96708
shahsuresh <shahsuresh75@gmail.com>
2024-09-01 21:18:03 +0545

    login route for admin is added

--- src/controllers/admin.routes.js
+++ src/controllers/admin.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import validateReqBody from "../middlewares/validation.middleware.js";
-import { adminDataValidationSchema } from "../validations/admin.validation.js";
+import {
+  adminDataValidationSchema,
+  adminLoginDataValidationSchema,
+} from "../validations/admin.validation.js";
 import Admin from "../models/admin.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = Router();
 
-//?============Register Admin===================
+//?============Register Admin=============================
 router.post(
   "/admin/register",
   validateReqBody(adminDataValidationSchema),
@@ -33,6 +37,46 @@ router.post(
     return res.status(200).send({ message: "Registration Success" });
   }
 );
+
+//?============ Login as Admin ===========================
+router.post(
+  "/admin/login",
+  validateReqBody(adminLoginDataValidationSchema),
+  async (req, res) => {
+    //extract email and password from req.body
+    const { email, password } = req.body;
+    //search user with provided email
+    const user = await Admin.findOne({ email: email });
+    //if no user with this email,throw error
+    if (!user) {
+      return res
+        .status(400)
+        .send({ message: "The email or password you entered is incorrect." });
+    }
+    //check for password match
+    const hashedPassword = user.password;
+    const isPasswordMatch = await bcrypt.compare(password, hashedPassword);
+    //if wrong password,throw error
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .send({ message: "The email or password you entered is incorrect." });
+    }
+    //generate accessToken
+    const payload = { id: user._id, email: user.email };
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SIGNATURE);
+    //to hide user password
+    user.password = undefined;
+    //send response
+    return res.status(200).send({
+      message: `Login Success:Welcome ${user.firstName} ${user.lastName}`,
+      adminData: user,
+      accessToken,
+    });
+  }
+);
+
+//?============ edit profile/ admin data =================
 
 //export router
 export default router;
